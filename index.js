9b5a62c9e344c83728ba9708a4d03d4ab0b839bf
Victor Marczyk <marczykv@gmail.com>
2023-07-28 20:05:18 -0400

    added fs

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs');
+// // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "What is your first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is your last name?"
+        },
+        {
+            type: "input",
+            name: "age",
+            message: "What is your age?"
+        }
+    ])
+    .then((answers) => {
+
+
+        fs.writeFile('mynewfile3.txt', JSON.stringify(answers), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
+
+
+    })
+    .catch((error) => {
+        console.log(error);
+    });
