5fcb93fb9dcd8cc8e9c7661150a49b6968b4691b
Victor Marczyk <marczykv@gmail.com>
2023-08-03 23:14:34 -0400

    added badge functionality and table of contents

--- index.js
+++ index.js
@@ -52,34 +52,74 @@ const questions = [
 ];
 
 
+
+// ... (previous code)
+
 inquirer
     .prompt(questions)
     .then((answers) => {
-        var readMeText = 
-`# ${answers.title}
+        let licenseBadgeURL = "";
+        switch (answers.license) {
+            case "MIT":
+        licenseBadgeURL = "https://img.shields.io/badge/license-MIT-brightgreen.svg";
+        break;
+      case "Apache":
+        licenseBadgeURL = "https://img.shields.io/badge/license-Apache%202-blue.svg";
+        break;
+      case "ISC":
+        licenseBadgeURL = "https://img.shields.io/badge/license-ISC-blue.svg";
+        break;
+      case "GPL":
+        licenseBadgeURL = "https://img.shields.io/badge/license-GPL-blue.svg";
+        break;
+      case "LGPL":
+        licenseBadgeURL = "https://img.shields.io/badge/license-LGPL-blue.svg";
+        break;
+      case "MPL":
+        licenseBadgeURL = "https://img.shields.io/badge/license-MPL-orange.svg";
+        break;
+            default:
+                licenseBadgeURL = "";
+                break;
+        }
+        const readMeText = `
+# ${answers.title}
+![License](${licenseBadgeURL})
 ## Project Description        
 ${answers.description}
 
 ## Table of Contents
 - [Description](#project-description)
 - [Installation](#installation-instructions)
-
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Installation Instructions
 ${answers.installation}
 
-## Additional Questions
-GitHub Account [${answers.github}](https://github.com/${answers.github})
-Email [${answers.email}](mailto:${answers.email})
-        
-        `;
-        fs.writeFile('generatedreadme.md', readMeText
-         , function (err) {
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+
+## Contributing
+${answers.contributors}
+
+## Tests
+${answers.test}
+
+## Questions
+GitHub Account: [${answers.github}](https://github.com/${answers.github})
+Email: [${answers.email}](mailto:${answers.email})
+`;
+        fs.writeFile('generatedreadme.md', readMeText, function (err) {
             if (err) throw err;
             console.log('Saved!');
         });
-
-
     })
     .catch((error) => {
         console.log(error);
